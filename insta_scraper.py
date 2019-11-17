a39157b7547787f8aea03c4313598482f2ba0f94
bahmi <bahadurmisho@gmail.com>
2019-11-17 08:09:56 +0600

    Implement PEP8 guidelines

--- insta_scraper.py
+++ insta_scraper.py
@@ -9,22 +9,24 @@ import requests
 import shutil
 
 # load environment variables
-with open('config.json','r') as f:
+with open('config.json', 'r') as f:
     config = json.load(f)
 
 
 class App:
-    def __init__(self, username=config['user']['username'], password=config['user']['password'],
-                 target_username=config['user']['target_username'], path=config['user']['path']):
+    def __init__(self, username=config['user']['username'],
+                 password=config['user']['password'],
+                 target_username=config['user']['target_username'],
+                 path=config['user']['path']):
         self.username = username
         self.password = password
         self.target_username = target_username
         self.path = path
-        self.driver = webdriver.Chrome() # creates an instance of Chrome
+        self.driver = webdriver.Chrome()  # creates an instance of Chrome
         self.error = False
         self.main_url = 'https://instagram.com'
-        self.driver.get((self.main_url)) # opens the url
-        sleep(randint(1,3))
+        self.driver.get((self.main_url))  # opens the url
+        sleep(randint(1, 3))
         self.log_in()
         sleep(randint(1, 3))
         if self.error is False:
@@ -32,39 +34,40 @@ class App:
             sleep(randint(1, 2))
             self.open_target_profile()
             sleep(randint(1, 3))
-        if self.error is False: # to prevent from scrolling the news feed
+        if self.error is False:  # to prevent from scrolling the news feed
             self.scroll_down()
             sleep(randint(1, 3))
         if self.error is False:
-            if not os.path.exists(path): # create a directory if it doesn't exist
-                os.mkdir(path)
+            if not os.path.exists(path):
+                os.mkdir(path)  # create a directory if doesn't exist
             self.download_photos()
 
         self.driver.close()
 
     def log_in(self):
-        """
-        logs into instagram by typing the credentials into the input form
-        """
+        """logs into instagram by typing the credentials into the input form"""
         try:
-            login_button = self.driver.find_element_by_xpath("//span[@id='react-root']//p[@class='izU2O']/a")
+            login_button = self.driver.find_element_by_xpath(
+                "//span[@id='react-root']//p[@class='izU2O']/a")
             sleep(randint(1, 3))
             login_button.click()
             sleep(randint(1, 3))
             try:
-                username_input = self.driver.find_element_by_xpath("//input[@name='username']")
+                username_input = self.driver.find_element_by_xpath(
+                    "//input[@name='username']")
                 sleep(randint(1, 3))
-                username_input.send_keys(self.username) # inputs data into the form
+                username_input.send_keys(self.username)
                 sleep(randint(1, 3))
             except Exception:
                 self.error = True
                 print('Couldn\'t find the username input field')
             try:
-                password_input = self.driver.find_element_by_xpath("//input[@name='password']")
+                password_input = self.driver.find_element_by_xpath(
+                    "//input[@name='password']")
                 sleep(randint(1, 3))
                 password_input.send_keys(self.password)
                 sleep(randint(1, 3))
-                password_input.submit() # submits the form
+                password_input.submit()  # submits the form
             except Exception:
                 self.error = True
                 print('Couldn\'t find the password input field')
@@ -73,16 +76,15 @@ class App:
             print('Unable to locate the login button')
 
     def close_dialog_box(self):
-        """
-        closes the dialog box that pops up after log in
-        """
+        """closes the dialog box that pops up after log in"""
         try:
             sleep(randint(1, 2))
-            close_dialog = self.driver.find_element_by_xpath("//div[@class='mt3GC']/button[@class='aOOlW   HoLwm ']")
+            close_dialog = self.driver.find_element_by_xpath(
+                "//div[@class='mt3GC']/button[@class='aOOlW   HoLwm ']")
             sleep(randint(1, 2))
             close_dialog.click()
             sleep(randint(1, 2))
-        except:
+        except Exception:
             pass
 
     def open_target_profile(self):
@@ -91,7 +93,8 @@ class App:
         opens the profile
         """
         try:
-            search_bar = self.driver.find_element_by_xpath("//input[@placeholder='Search']")
+            search_bar = self.driver.find_element_by_xpath(
+                "//input[@placeholder='Search']")
             sleep(randint(1, 3))
             search_bar.send_keys(self.target_username)
             target_profile_url = self.main_url + '/' + self.target_username + '/'
@@ -103,17 +106,20 @@ class App:
 
     def scroll_down(self):
         """
-        scrolls down in the target profile to load all the images available
+        scrolls down in the target profile to load all
+        the images available
         """
         try:
-            no_of_posts = self.driver.find_element_by_xpath("//span[@class='g47SY ']") # finds the number of posts
+            no_of_posts = self.driver.find_element_by_xpath(
+                "//span[@class='g47SY ']")  # finds the number of posts
             no_of_posts = str(no_of_posts.text).replace(',', '')
             self.no_of_posts = int(no_of_posts)
             if self.no_of_posts > 12:
-                no_of_scrolls = int(self.no_of_posts/12) + 2 # calculate the number of times to scroll
+                no_of_scrolls = int(self.no_of_posts / 12) + 2
                 try:
                     for value in range(no_of_scrolls):
-                        self.driver.execute_script('window.scrollTo(0, document.body.scrollHeight);') # scrolls down
+                        self.driver.execute_script(
+                            'window.scrollTo(0, document.body.scrollHeight);')
                         sleep(randint(3, 4))
                 except Exception:
                     self.error = True
@@ -124,26 +130,24 @@ class App:
             print('Couldn\'t find number of posts while scrolling')
 
     def download_photos(self):
-        """
-        download images from the target profile
-        """
+        """download images from the target profile"""
         soup = BeautifulSoup(self.driver.page_source, 'lxml')
         all_images = soup.find_all('img')
         print('number of photos: ', len(all_images))
 
         for index, image in enumerate(all_images):
             file_name = 'image_' + str(index) + '.jpg'
-            image_path = os.path.join(self.path, file_name) # create image path
+            image_path = os.path.join(self.path, file_name)  # create image path
             link = image['src']
             print('Downloading image #' + str(index))
             try:
                 response = requests.get(link, stream=True)  # get the image
                 with open(image_path, 'wb') as file:
-                    shutil.copyfileobj(response.raw, file) # save image to storage device
+                    shutil.copyfileobj(response.raw, file)  # save image
             except requests.exceptions.MissingSchema:
                 print('Couldn\'t download image #' + str(index))
                 print('image link: ' + str(link))
 
 
 if __name__ == '__main__':
-    app = App() # creates an App instance
+    app = App()  # creates an App instance
